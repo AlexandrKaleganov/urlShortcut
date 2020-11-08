22cd8f520a47276dde196d525c6585a786c2bca8
Alexander Kaleganov <alexmur07@mail.ru>
2020-11-08 11:43:29 +0700

    https append

--- src/app/shared/constant/url.constant.ts
+++ src/app/shared/constant/url.constant.ts
@@ -1,1 +1,3 @@
-export const GLOBAL_URL = 'http://' + location.hostname + ':9000';
+// export const GLOBAL_URL = 'http://' + location.hostname + ':8080';
+// export const GLOBAL_URL = 'http://' + location.hostname;
+export const GLOBAL_URL = 'https://' + location.hostname;
